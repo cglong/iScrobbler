7f5178b2b776e73f44d40bdac422940a1f85c9fe
brian <brian@5af56264-5426-0410-a0e3-f56194a38e4e>
2007-05-11 17:45:06 +0000

    nil ivars in [close]

--- src/BBNetUpdate/BBNetUpdateVersionCheckController.m
+++ src/BBNetUpdate/BBNetUpdateVersionCheckController.m
@@ -1,5 +1,5 @@
 /*
-* Copyright 2002,2006 Brian Bergstrand.
+* Copyright 2002,2006,2007 Brian Bergstrand.
 *
 * Redistribution and use in source and binary forms, with or without modification, 
 * are permitted provided that the following conditions are met:
@@ -194,7 +194,9 @@ __private_extern__ NSString *BBNetUpdateDidFinishUpdateCheck = @"BBNetUpdateDidF
 - (void)close;
 {
    [verInfo release];
+   verInfo = nil;
    [source release];
+   source = nil;
    
    [[NSUserDefaults standardUserDefaults]
       setBool:(NSOnState == [boxDontCheck state]) forKey:@"BBNetUpdateDontAutoCheckVersion"];
