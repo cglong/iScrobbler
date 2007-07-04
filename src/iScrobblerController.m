e71c3cdb7ab5576f1c627c4bc8c5f66a50f01ac4
brian <brian@5af56264-5426-0410-a0e3-f56194a38e4e>
2007-07-04 20:34:05 +0000

    Bug Fix: Remove extra colon in menu tool for the subs count.

--- src/iScrobblerController.m
+++ src/iScrobblerController.m
@@ -111,7 +111,7 @@ static void handlesig (int sigraised)
             msg = [items objectAtIndex:0];
     } else {
         msg = [NSString stringWithFormat:@"%@: %u",
-                NSLocalizedString(@"Subs:", "Tracks Submitted Abbreviation"), [[QueueManager sharedInstance] totalSubmissionsCount]];
+                NSLocalizedString(@"Subs", "Tracks Submitted Abbreviation"), [[QueueManager sharedInstance] totalSubmissionsCount]];
         if (tracksQueued = [[QueueManager sharedInstance] count]) {
             msg = [msg stringByAppendingFormat:@", %@: %u",
                 NSLocalizedString(@"Q'd", "Tracks Queued Abbreviation"), tracksQueued];
