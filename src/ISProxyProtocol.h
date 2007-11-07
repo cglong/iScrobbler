d3c7d8f54c7b56c03a37ee718ff40805e8757825
brian <brian@5af56264-5426-0410-a0e3-f56194a38e4e>
2007-11-07 00:18:35 +0000

    Fix comment sp.

--- src/ISProxyProtocol.h
+++ src/ISProxyProtocol.h
@@ -9,7 +9,7 @@
 //
 
 // There are serious Applescript bugs in 10.5.0 for 64bit apps.
-// ISProxy is simplye a 32 bit proxy to run problematic scripts until the issues are fixed.
+// ISProxy is simply a 32 bit proxy to run problematic scripts until the issues are fixed.
 // (Hopefully in 10.5.1, but I'm not counting on it.)
 
 #import <Cocoa/Cocoa.h>
