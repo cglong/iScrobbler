73bb6ec56f5c1baa02bd14b5f9f12db1e3d10ce5
brian <brian@5af56264-5426-0410-a0e3-f56194a38e4e>
2008-03-11 14:04:21 +0000

    Added sandbox support to block loading of input managers.

--- src/main.m
+++ src/main.m
@@ -7,6 +7,10 @@
 //
 
 #import <Cocoa/Cocoa.h>
+#import <sandbox.h>
+#if MAC_OS_X_VERSION_MIN_REQUIRED < MAC_OS_X_VERSION_10_5
+#pragma weak sandbox_init
+#endif
 
 #if MAC_OS_X_VERSION_MIN_REQUIRED < MAC_OS_X_VERSION_10_5
 @interface ISTigerAnimationViewProxy : NSView {
@@ -27,6 +31,26 @@
 
 int main(int argc, const char *argv[])
 {
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    
+    #if MAC_OS_X_VERSION_MIN_REQUIRED < MAC_OS_X_VERSION_10_5
+    if (sandbox_init)
+    #endif
+    {
+        const char *sbf = [[[NSBundle mainBundle] pathForResource:@"iScrobbler" ofType:@"sb"] fileSystemRepresentation];
+        if (sbf) {
+            char *sberr = NULL;
+            (void)sandbox_init(sbf, SANDBOX_NAMED_EXTERNAL, &sberr);
+            if (sberr) {
+                #ifdef ISDEBUG
+                if (strlen(sberr) > 0)
+                    NSLog(@"sandbox error: '%s'\n", sberr);
+                #endif
+                sandbox_free_error(sberr);
+            }
+        }
+    }
+
     #if MAC_OS_X_VERSION_MIN_REQUIRED < MAC_OS_X_VERSION_10_5
     if (floor(NSFoundationVersionNumber) <= NSFoundationVersionNumber10_4) {
         // this allows us to use [view animator] throught out the code w/o adding a runtime check for each instance
@@ -36,6 +60,8 @@ int main(int argc, const char *argv[])
         [ISTigerAnimationViewProxy poseAsClass:[NSView class]];
     }
     #endif
+    
+    [pool release];
     
     return NSApplicationMain(argc, argv);
 }
