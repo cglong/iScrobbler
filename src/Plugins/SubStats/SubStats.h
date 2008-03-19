2bef76b696336b885c1baf8f89052da77b99660a
brian <brian@5af56264-5426-0410-a0e3-f56194a38e4e>
2008-03-19 13:42:30 +0000

    Added SubStats plugin.

---
+++ src/Plugins/SubStats/SubStats.h
@@ -0,0 +1,16 @@
+//
+//  SubStats.h
+//  iScrobbler Plugin
+//
+//  Copyright 2008 Brian Bergstrand.
+//
+//  Released under the GPL, license details available in res/gpl.txt
+//
+#import <Cocoa/Cocoa.h>
+#import "ISPlugin.h"
+
+@interface ISSubStats : NSObject <ISPlugin> {
+    id mProxy;
+}
+
+@end
