b7f9ef64dced8f563e8bb2c94f880230eca8d5f2
brian <brian@5af56264-5426-0410-a0e3-f56194a38e4e>
2008-11-30 23:23:35 +0000

    Code cleanup

--- src/ISiTunesLibrary.m
+++ src/ISiTunesLibrary.m
@@ -149,7 +149,7 @@
     [[didEnd target] performSelectorOnMainThread:[didEnd selector] withObject:upcallArg waitUntilDone:NO];
 }
 
-- (void)readiTunesLibThread:(id)arg
+- (void)iTunesLibThread:(id)arg
 {
     NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
     
@@ -194,7 +194,7 @@
         }
     }
     if (createth) {
-        [NSThread detachNewThreadSelector:@selector(readiTunesLibThread:) toTarget:self withObject:self];
+        [NSThread detachNewThreadSelector:@selector(iTunesLibThread:) toTarget:self withObject:self];
         useconds_t uWait = 0;
         id msg;
         do {
